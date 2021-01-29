39ede06babe3f97ec3bb5536a95e2b3797bc60d4
Zachary Munson <zmunson85@gmail.com>
2021-01-29 04:22:55 -0700

    initial commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+    FaFacebook,
+    FaInstagram,
+    FaYoutube,
+    FaGithub,
+    FaLinkedin
+} from 'react-icons/fa';
+import {
+    FooterContainer,
+    FooterWrap,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcons,
+    SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>Late Night Pizza Shop</SocialLogo>
+                        <SocialIcons>
+                            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink
+                                href='//www.github.com/zmunson85'
+                                target='_blank'
+                                aria-label='Github'
+                                rel='noopener noreferrer'
+                            >
+                                <FaGithub />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Hero/index.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import Navbar from '../Navbar'
+import  Sidebar  from '../Sidebar';
+import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroElements'
+
+const Hero = () => {
+    const[isOpen, setIsOpen] = useState(false)
+    const toggle = () =>{
+        setIsOpen(!isOpen)
+    }
+    return (
+        <HeroContainer>
+            <Navbar toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Great Pizza Open late!</HeroH1>
+                    <HeroP>Delivery Available</HeroP>
+                    <HeroBtn>Place Order</HeroBtn>
+                </HeroItems>
+            </HeroContent>
+        </HeroContainer>
+
+    )
+}
+
+export default Hero;
